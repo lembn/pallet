42cc51d582992f45472931f0a7e77d871916b20d
lembn <lembn@outlook.com>
2022-03-07 23:55:31 +0000

    commit

--- src/notes/Note.java
+++ src/notes/Note.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Random;
+import java.util.function.Consumer;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.exc.StreamWriteException;
@@ -13,36 +14,47 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Note {
     private static ObjectMapper mapper = new ObjectMapper();
+    private static Consumer<Integer> onDelete;
 
     private int id;
+    private String address;
     private String title;
     private String para;
     private Date lastEdited;
     private boolean isOwned;
     private boolean isPrivate;
+    private String path;
 
     @JsonCreator
-    public Note(int id, String title, String para, Date lastEdited, boolean isOwned,
-            boolean isPrivate) {
+    public Note(int id, String address, String title, String para, Date lastEdited, boolean isOwned,
+            boolean isPrivate, String path) {
         this.id = id;
+        this.address = address;
         this.title = title;
         this.para = para;
         this.lastEdited = lastEdited;
         this.isOwned = isOwned;
         this.isPrivate = isPrivate;
+        this.path = String.format("%s/%s.json", path, this);
     }
 
     public Note(String path) throws JsonMappingException, JsonProcessingException {
         Note note = mapper.readValue(path, Note.class);
+        this.id = note.id;
         this.title = note.title;
         this.para = note.para;
         this.lastEdited = note.lastEdited;
         this.isOwned = note.isOwned;
         this.isPrivate = note.isPrivate;
+        this.path = path;
     }
 
     public int id() {
         return id;
+    }
+
+    public String address() {
+        return address;
     }
 
     public String title() {
@@ -65,36 +77,29 @@ public class Note {
         return isPrivate;
     }
 
-    public void save(String path) throws StreamWriteException, DatabindException, IOException {
-        File outfile = new File(String.format("%s/%s.json", path, this));
+    public void togglePrivacy() throws StreamWriteException, DatabindException, IOException {
+        this.isPrivate = !this.isPrivate;
+        save();
+    }
+
+    public void save() throws StreamWriteException, DatabindException, IOException {
+        // TODO: debounce
+        File outfile = new File(path);
         if (!outfile.exists())
             outfile.createNewFile();
         mapper.writeValue(outfile, this);
     }
 
-    public void delete(String path) {
-        File outfile = new File(String.format("%s/%s.json", path, this));
+    public void delete() {
+        File outfile = new File(path);
         if (outfile.exists())
             outfile.delete();
+        onDelete.accept(id);
     }
 
     @Override
     public String toString() {
         return Integer.toHexString(id);
-    }
-
-    @Override
-    public int hashCode() {
-        return id;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Note) {
-            Note other = (Note) obj;
-            return id == other.id;
-        } else
-            return false;
     }
 
     // TODO: this will go on forever if it never find a free id
@@ -107,5 +112,9 @@ public class Note {
             if (!file.exists())
                 return id;
         }
+    }
+
+    public static void setOnDelete(Consumer<Integer> onDelete) {
+        Note.onDelete = onDelete;
     }
 }
